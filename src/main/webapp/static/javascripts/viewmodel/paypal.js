e522c8f765d560ae1b121b220f4b5b0053f5c537
Sivan Brezniak <sivanb@wix.com>
2012-08-30 17:00:20 +0300

    pay pal

---
+++ src/main/webapp/static/javascripts/viewmodel/paypal.js
@@ -0,0 +1,16 @@
+var ViewModel = function(instanceToken) {
+    var self = this;
+    self.payKey = ko.observable(null);
+
+    self.preparePayment = function() {
+        $.ajax({
+            url: "/api/v1/chat/prepare-payment?instance=" + instanceToken +"&returnUrl=" + window.location.href,
+            type: "POST",
+            dataType: "json",
+            contentType: "application/json",
+            success: function(response) {
+                self.payKey = response.payKey;
+            }
+        })
+    }
+};
